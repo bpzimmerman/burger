28475dc581532efc10ad7ebf5be0ca2436fcdfeb
bpzimmerman <32779243+bpzimmerman@users.noreply.github.com>
2018-03-27 15:50:14 -0400

    fixed typo in comment

--- models/burger.js
+++ models/burger.js
@@ -24,5 +24,5 @@ var burger = {
   }
 };
 
-// Export the database functions for the controller (catsController.js).
+// Export the database functions for the controller (burgerController.js).
 module.exports = burger;
